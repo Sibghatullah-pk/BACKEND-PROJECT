ed81d83ea50138fb405a5b7527a64b2535acf2e9
Sibghatullah-pk <sibghatullah1a2a3a@gmail.com>
2025-08-13 15:43:47 +0500

    g

--- server.js
+++ server.js
@@ -9,6 +9,11 @@ res.render("homepage")
 app.use(express.static("public"))
 })
 
+app.get("/login", (req, res) => {
+    res.render("login")
+})
+
+
 app.listen(3000, () => {
   console.log("Server is running on port 3000");
 })
